7ddc9cb5e59ad288f84eda285a3f95e841726978
reckless-sherixx <ezboi2312@gmail.com>
2025-07-19 01:31:07 +0530

    LeetCode

--- LeetCode/Q1920.java
+++ LeetCode/Q1920.java
@@ -1,11 +1,26 @@
 package LeetCode;
 
-public class Q1920{
+public class Q1920 {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
-          for(int i = 0 ; i< nums.length ; i++){
-            ans[i] = nums[nums[i]];
-          }
-          return ans;
+        // TS: O(n) , SC: O(1);
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] += n * (nums[nums[i]] % n);
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] /= n;
+        }
+
+        return nums;
+
+        // TS: O(n) , SC: O(n);
+
+        // int[] ans = new int[nums.length];
+        // for(int i = 0 ; i< nums.length ; i++){
+        // ans[i] = nums[nums[i]];
+        // }
+        // return ans;
     }
 }
