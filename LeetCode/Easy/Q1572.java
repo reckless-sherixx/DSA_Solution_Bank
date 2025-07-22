4e309bc6bad56013dbff3d471f1cdcd3469013b4
reckless-sherixx <ezboi2312@gmail.com>
2025-07-23 00:22:50 +0530

    LeetCode

---
+++ LeetCode/Easy/Q1572.java
@@ -0,0 +1,16 @@
+package LeetCode.Easy;
+
+public class Q1572 {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+
+            if (i != n - 1 - i) {
+                sum += mat[i][n - 1 - i];
+            }
+        }
+        return sum;
+    }
+}
