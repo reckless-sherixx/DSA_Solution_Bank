97c69f90d221115c76d3ba81563d3d6e0330dc71
reckless-sherixx <ezboi2312@gmail.com>
2025-07-26 00:50:01 +0530

    LeetCode

--- LeetCode/Easy/Q1572.java
+++ LeetCode/Easy/Q1572.java
@@ -7,6 +7,7 @@ public class Q1572 {
         for (int i = 0; i < n; i++) {
             sum += mat[i][i];
 
+            // check 
             if (i != n - 1 - i) {
                 sum += mat[i][n - 1 - i];
             }
