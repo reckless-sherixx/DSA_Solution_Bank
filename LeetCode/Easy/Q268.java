728ec1561b153233eb8ec59a129e8c435d04d3df
reckless-sherixx <ezboi2312@gmail.com>
2025-07-29 06:49:54 +0530

    LeetCode

---
+++ LeetCode/Easy/Q268.java
@@ -0,0 +1,13 @@
+package LeetCode.Easy;
+
+public class Q268 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int totalSum = n * (n + 1) / 2;
+        int currentSum = 0;
+        for (int i = 0; i < n; i++) {
+            currentSum += nums[i];
+        }
+        return totalSum - currentSum;
+    }
+}
---
+++ LeetCode/Easy/Q448.java
@@ -0,0 +1,36 @@
+package LeetCode.Easy;
+
+import java.util.*;
+
+public class Q448 {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        cyclicSort(nums);
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                ans.add(i + 1);
+            }
+        }
+        return ans;
+    }
+
+    void cyclicSort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
