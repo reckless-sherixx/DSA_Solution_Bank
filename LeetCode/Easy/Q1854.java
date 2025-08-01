1d3af1660e6deca41b735e20042af08639de1f4e
reckless-sherixx <ezboi2312@gmail.com>
2025-08-01 20:55:38 +0530

    LeetCode

---
+++ LeetCode/Easy/Q1854.java
@@ -0,0 +1,25 @@
+package LeetCode.Easy;
+
+public class Q1854 {
+    public int maximumPopulation(int[][] logs) {
+        int[] year = new int[2051];
+
+        for (int[] log : logs) {
+            year[log[0]] += 1;
+            year[log[1]] -= 1;
+
+        }
+
+        int maxNum = year[1950], maxYear = 1950;
+
+        for (int i = 1951; i < year.length; i++) {
+            year[i] += year[i - 1];
+
+            if (year[i] > maxNum) {
+                maxNum = year[i];
+                maxYear = i;
+            }
+        }
+        return maxYear;
+    }
+}
---
+++ LeetCode/Easy/Q566.java
@@ -0,0 +1,17 @@
+package LeetCode.Easy;
+
+public class Q566 {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        if (m * n != r * c)
+            return mat;
+
+        int[][] res = new int[r][c];
+        for (int i = 0; i < m * n; i++) {
+            res[i / c][i % c] = mat[i / n][i % n];
+        }
+        return res;
+    }
+}
