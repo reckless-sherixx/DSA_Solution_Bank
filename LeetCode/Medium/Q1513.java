bbb54a6b638e47481fd7313cf791f0d349702614
reckless-sherixx <ezboi2312@gmail.com>
2025-08-22 03:05:45 +0530

    LeetCode

---
+++ LeetCode/Medium/Q1513.java
@@ -0,0 +1,12 @@
+package LeetCode.Medium;
+
+public class Q1513 {
+    public int numSub(String s) {
+         int res = 0 , count = 0 , mod = (int)1e9+7;
+         for(int i = 0 ; i < s.length() ; i++){
+            count = s.charAt(i) == '1' ? count + 1 : 0;
+            res = ( res + count ) % mod ;
+         }
+         return res;
+    }
+}
