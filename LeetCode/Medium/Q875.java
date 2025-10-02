5e2179c39dfae5cac6e834037dd8b22ffbf9bf2d
reckless-sherixx <ezboi2312@gmail.com>
2025-10-02 22:58:04 +0530

    LeetCode

--- LeetCode/Medium/Q875.java
+++ LeetCode/Medium/Q875.java
@@ -1,28 +1,32 @@
 package LeetCode.Medium;
 
+import java.util.*;
+
 public class Q875 {
     public int minEatingSpeed(int[] piles, int h) {
-        int s = 1 ; int e = Arrays.stream(piles).max().getAsInt();
+        int s = 1;
+        int e = Arrays.stream(piles).max().getAsInt();
         int ans = e;
-        while(s <= e){
-            int m = s + ( e - s) / 2;    
-            if(canFinish(piles,h ,m)){
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            if (canFinish(piles, h, m)) {
                 ans = m;
                 e = m - 1;
-            }
-            else{
+            } else {
                 s = m + 1;
-            }                        
+            }
         }
         return ans;
 
     }
-    public boolean canFinish(int[] piles , int h , int k){
+
+    public boolean canFinish(int[] piles, int h, int k) {
         long hours = 0;
-        for(int pile: piles){
-            hours+=pile/k;
-            if(pile% k!=0) hours ++;
+        for (int pile : piles) {
+            hours += pile / k;
+            if (pile % k != 0)
+                hours++;
         }
-        return hours <=h;
+        return hours <= h;
     }
 }
