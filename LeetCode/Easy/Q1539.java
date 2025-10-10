4a6a2563e246964e10a66da37c18c99df0dd9fce
reckless-sherixx <ezboi2312@gmail.com>
2025-10-11 00:18:51 +0530

    LeetCode

---
+++ LeetCode/Easy/Q1539.java
@@ -0,0 +1,16 @@
+package LeetCode.Easy;
+
+public class Q1539 {
+    public int findKthPositive(int[] arr, int k) {
+        int s = 0 ; int e = arr.length - 1;
+        while(s <= e){
+            int m = s + ( e - s ) / 2;
+            if(arr[m] - m - 1 < k){
+                s = m + 1;
+            }else {
+                e = m - 1;
+            }
+        }
+        return s + k;
+    }
+}
