8cb3437c452d6c1ba304de37e18ad834093e4ddf
Vidyansh <ezboi2312@gmail.com>
2026-04-25 01:58:50 +0530

    LeetCode

---
+++ LeetCode/Python/Easy/Q26.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        if len(nums) == 0:
+            return 0
+        j = 0
+        for i in range(0 , len(nums)):
+            if nums[j] != nums[i]:
+                j+=1
+                nums[j] = nums[i]
+            
+        return j+1
