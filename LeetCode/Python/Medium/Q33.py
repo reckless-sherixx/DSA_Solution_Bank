4c3f265bb3a65b6832a33cc33923dec83e7d975f
Vidyansh <ezboi2312@gmail.com>
2026-05-08 03:39:53 +0530

    LeetCode

---
+++ LeetCode/Python/Medium/Q33.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums, target) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] <= nums[end]:
+                if nums[mid] <= target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            else:
+                if nums[start] <= target <= nums[mid]:
+                    end = mid - 1
+                else: 
+                    start = mid + 1
+        return -1 
